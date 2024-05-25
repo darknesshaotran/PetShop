872ae1577645d8c866010296c68f7a61b60aeb48
darknesshaotran <darknessclmm@gmail.com>
2024-05-26 00:10:56 +0700

    set up Oauth2.0

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const route = require('./routes/index.js');
 const conenctionDatabase = require('../connect_db.js');
 const ErrorHandler = require('../src/middlewares/error.middlewares.js');
 const dotenv = require('dotenv');
+const passport = require('./utils/passport.js');
 const initSocket = require('./utils/socket.js');
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 conenctionDatabase();
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'], session: false }));
+
+app.get(
+    '/auth/google/callback',
+    (req, res, next) => {
+        passport.authenticate('google', (err, profile) => {
+            const Profile = JSON.parse(JSON.stringify(profile));
+            req.user = Profile;
+            next();
+        })(req, res, next);
+    },
+    (req, res) => {
+        res.json(req.user.id);
+    },
+);
 route(app);
 
 // note : dat middlewares error handler tai noi cuoi cung
---
+++ src/utils/passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const dotenv = require('dotenv');
+dotenv.config();
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: '/auth/google/callback',
+        },
+        function (accessToken, refreshToken, profile, cb) {
+            return cb(null, profile);
+        },
+    ),
+);
+module.exports = passport;
